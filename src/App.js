0726871c342bcc6d25f702afe0c409da5a3c729e
Mart1n991 <martinjavorsky8@gmail.com>
2021-03-02 08:38:39 +0100

    vytvorenie svg obrázkov square a circle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import square from "./assets/square.svg";
+
 function App() {
-  return <div className="App">Lotka prepočty</div>;
+  return (
+    <div className="App">
+      Lotka prepočty
+      <img src={square} alt="štvorec" />
+    </div>
+  );
 }
 
 export default App;
